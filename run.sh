e2fa035b1cf00ae6b837fd7de81536dd6902cfd9
Joseph McAlear <Joe.mcalear@gmail.com>
2025-12-11 00:43:09 -0500

    updated run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,11 @@ sudo apt install git
 
 sudo apt install docker-compose
 
-git pull
+git pull https://github.com/JosephMcAlear/Home-Automation-Server.git
+
+cd Home-Automation-Server
+
+git checkout deploy
+
+docker-compose build
+
